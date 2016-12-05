c179d5b0e6a47b9de77239517af1ad85f27239a9
Ben Harper <rogojin@gmail.com>
2016-12-05 14:21:30 +0200

    Add new copy_out build phase to imqs-build.rb

--- imqs-build.rb
+++ imqs-build.rb
@@ -26,6 +26,7 @@ end
 case ARGV[0]
 	when "prepare"	then
 		exec_or_die("go install github.com/IMQS/scheduler/cmd")
+	when "copy_out"	then
 		FileUtils.cp("bin/cmd.exe", out_dir + '/bin/imqsscheduler.exe')
 	when "test_unit" then
 	when "test_integration" then
